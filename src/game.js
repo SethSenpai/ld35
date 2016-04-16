44307fad68ce24289a930a5c34b801dc3fa99337
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2016-04-16 10:44:28 +0200

    forgot some files

---
+++ src/game.js
@@ -0,0 +1,19 @@
+/// <reference path="third-party/phaser.d.ts"/>
+
+var game = new Phaser.Game(800, 600, Phaser.WEBGL, '', {
+  preload: preload, 
+  create: create,
+  update: update
+});
+  
+function preload() {
+  game.load.image('logo', 'phaser.png');
+}
+
+function create() {
+  var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+  logo.anchor.setTo(0.5, 0.5);
+}
+
+function update() {
+}
