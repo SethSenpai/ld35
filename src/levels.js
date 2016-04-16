fdb4d29d661360b1f3a80d17b588ea23c385664e
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2016-04-16 21:32:52 +0200

    for real

--- src/levels.js
+++ src/levels.js
@@ -1,10 +1,17 @@
 var LEVEL = {
   ONE: {
+    start: { x: WIDTH / 8, y: HEIGHT / 2 },
+    end: { x: WIDTH * 7 / 8, y: HEIGHT / 2 },
     vertices: [
-      { x: 10, y: HEIGHT / 3, fixed: true },
+      { x: -20, y: HEIGHT / 3, fixed: true },
+      { x: WIDTH / 4, y: HEIGHT / 3, fixed: false },
+      { x: WIDTH / 3, y: HEIGHT * 2 / 3, fixed: false },
       { x: WIDTH / 2, y: HEIGHT / 3 - 20, fixed: false },
-      { x: WIDTH - 10, y: HEIGHT / 3 + 20, fixed: true },
-      { x: WIDTH * 2 / 3, y: HEIGHT * 2 / 3, fixed: false }
+      { x: WIDTH + 20, y: HEIGHT / 3 + 20, fixed: true },
+      { x: WIDTH + 20, y: HEIGHT * 4 / 5, fixed: true },
+      { x: WIDTH * 2 / 3, y: HEIGHT * 2 / 3, fixed: false },
+      { x: WIDTH / 3, y: HEIGHT * 2 / 3 + 40, fixed: false },
+      { x: -20, y: HEIGHT * 2 / 3, fixed: true }
     ]
   }
 };
--- src/main.js
+++ src/main.js
@@ -10,35 +10,51 @@ var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.WEBGL, '', {
   update: update
 });
 
+var player;
+var recepticle;
+
 function preload() { 
   game.load.image('water', 'sprites/water_molecule_small.png');
+  game.load.image('recepticle', 'sprites/recepticle_small.png');
   game.load.image('background', 'sprites/background.png');
   
   game.load.physics('water-data', 'sprites/water_molecule_small.json');
+  game.load.physics('recepticle-data', 'sprites/recepticle_small.json');
   
   Membrane.preload(game);
 }
 
 function create() {
+  var level = LEVEL.ONE;
+
   game.physics.startSystem(Phaser.Physics.P2JS);
-  game.physics.p2.restitution = 0.9;
-  game.physics.p2.gravity.y = 500;
+  game.physics.p2.restitution = 0.8;
+  //game.physics.p2.gravity.y = 500;
   
   game.add.image(0, 0, 'background');
   
-  Membrane.create(game, LEVEL.ONE);
+  Membrane.create(game, level);
   
-  // Add demo molecules
-  var balls = game.add.physicsGroup(Phaser.Physics.P2JS);
-  for (var i = 0; i < 20; i++) {
-    var ball = balls.create(Math.random() * WIDTH, Math.random() * HEIGHT, 'water');
-    
-    game.physics.p2.enableBody(ball, true);
-    
-    ball.body.clearShapes();
-    ball.body.loadPolygon('water-data', 'water_molecule_small');
-  }
+  // Add main 'player' molecule
+  player = game.add.sprite(level.start.x, level.start.y, 'water');
+  game.physics.p2.enableBody(player, DEBUG);
+  player.body.clearShapes();
+  player.body.loadPolygon('water-data', 'water_molecule_small');
+
+  // Add recepticle
+  recepticle = game.add.sprite(level.end.x, level.end.y, 'recepticle');
+  game.physics.p2.enableBody(recepticle, DEBUG);
+  recepticle.body.clearShapes();
+  recepticle.body.loadPolygon('recepticle-data', 'recepticle_small');
+  recepticle.body.static = true;
 }
 
 function update() {
+
+  // Accelerate player to recepticle
+  var factor = 60;
+  var angle = Math.atan2(recepticle.y - player.y, recepticle.x - player.x);
+  player.body.rotation = angle + game.math.degToRad(90);
+  player.body.force.x = Math.cos(angle) * factor;
+  player.body.force.y = Math.sin(angle) * factor;
 }
